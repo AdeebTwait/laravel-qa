6c7151f5d1628dc6692483a305169e9b02e87999
Adeeb Twait <adeebtwait@gmail.com>
2019-04-26 18:02:23 +0300

    Make the favorite button works

--- app/Question.php
+++ app/Question.php
@@ -19,6 +19,27 @@ class Question extends Model
         return $this->hasMany(Answer::class);
     }
 
+
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
+
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
+    }
+
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
@@ -56,4 +77,5 @@ class Question extends Model
         $this->best_answer_id = $answer->id;
         $this->save();
     }
+
 }
--- app/User.php
+++ app/User.php
@@ -36,6 +36,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Answer::class);
     }
+
+    public function favorites()
+    {
+        return $this->belongsToMany(Question::class, 'favorites')->withTimestamps();
+    }
     
     public function getUrlAttribute()
     {
